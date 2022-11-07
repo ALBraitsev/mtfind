8cd5c123fb5f6bbddca2d2875a6fa007e66f8570
balou <a.braytsev@game-insight.com>
2022-11-07 20:09:09 +0500

    поиск без предварительного разбиения на строки;

--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,9 @@
 #include <future>
 #include <tuple>
 
-enum class MTFindResult
-{
-    OK,
-    FAILURE
-};
+typedef std::tuple<int, int, std::string_view> MTMathResult;
+typedef std::vector<MTMathResult> MTMathResults;
+typedef std::tuple<long, MTMathResults> MTMathInPartResults;
 
 void usage(const char *programName)
 {
@@ -22,126 +20,119 @@ void usage(const char *programName)
               << std::endl;
 }
 
-MTFindResult readFileToBuffer(const char *fileName, char *&buffer)
+long readFileToBuffer(const char *fileName, char **buffer)
 {
     FILE *fp;
     long lSize;
 
     fp = fopen(fileName, "rb");
     if (!fp)
-        return MTFindResult::FAILURE;
+        return -1;
 
     fseek(fp, 0L, SEEK_END);
     lSize = ftell(fp);
-    rewind(fp);
+    fseek(fp, 0L, SEEK_SET);
 
-    /* allocate memory for entire content */
-    buffer = (char *)malloc(lSize + 1);
+    *buffer = (char *)malloc(lSize);
     if (!buffer)
     {
         fclose(fp);
-        return MTFindResult::FAILURE;
+        return -2;
     }
 
-    /* copy the file into the buffer */
-    if (1 != fread(buffer, lSize, 1, fp))
+    if (fread(*buffer, lSize, 1, fp) != 1)
     {
         fclose(fp);
         free(buffer);
-        return MTFindResult::FAILURE;
+        return -3;
     }
-    buffer[lSize] = 0;
     
-    /* do your work here, buffer is a string contains the whole text */
     fclose(fp);
 
-    return MTFindResult::OK;
+    return lSize;
 }
 
-void splitBufferToLines(const char *buffer, char ld, std::vector<std::string_view> &lines)
+MTMathInPartResults
+findInPart(const char* text, const char* textEnd, const char* pattern, size_t patternSize)
 {
-    const char *pb = buffer;
-    const char *pe = pb;
-    while (*pe)
+    MTMathInPartResults result(0, {});
+    const char* t = text;
+    const char* p = pattern;
+    const char* pe = pattern + patternSize;
+    char c = 0;
+    while(text < textEnd) 
     {
-        if (*pe == ld)
+        c = *(text++); 
+
+        if (c == '\n')
         {
-            lines.emplace_back(pb, pe - pb);
-            pb = pe + 1;
+            ++std::get<0>(result);
+            t = text;
+            p = pattern;
         }
-        ++pe;
+        else
+        {
+            if (p < pe)
+            {
+                if (*p == c || *p == '?')
+                {
+                    ++p;
+                }
+                else
+                {
+                    text -= (p - pattern);
+                    p = pattern;
+                }
+            }
+            else
+            {
+                --text;
+                std::get<1>(result).emplace_back(
+                    static_cast<int>(std::get<0>(result)), 
+                    static_cast<int>((text - patternSize) - t), 
+                    std::string_view{(text - patternSize), static_cast<std::string_view::size_type>(patternSize)}
+                );
+                p = pattern;
+            }
+        }
     }
-    lines.emplace_back(pb, pe - pb);
+    return result;
 }
 
-inline 
-const char *bruteforcematch(const char *string, int stringSize,
-                        const char *pattern, int patternSize)
+MTMathResults 
+find(const char* text, size_t textSize, const char* pattern, size_t patternSize, int parts) 
 {
-    if (patternSize > stringSize)
-        return nullptr;
+    std::vector<std::future<MTMathInPartResults>> futures;
 
-    int i, j;
-    for (j = 0; j <= stringSize - patternSize; ++j)
+    size_t partSize = textSize / parts;
+    const char* textEnd = text + textSize;
+
+    const char* part = text;
+    const char* partEnd = part + partSize;
+    while (part < textEnd) 
     {
-        for (i = 0; i < patternSize && (pattern[i] == '?' || pattern[i] == string[i + j]); ++i)
+        while (partEnd < textEnd && *partEnd++ != '\n')
             ;
-        if (i >= patternSize)
-            return string + j;
+
+        futures.push_back(std::async(std::launch::async, findInPart, part, partEnd, pattern, patternSize));
+
+        part = partEnd;
+        partEnd = part + partSize;
     }
 
-    return nullptr;
-}
-
-std::vector<std::tuple<int, int, std::string_view>> 
-findInRange(std::vector<std::string_view>::iterator begin, std::vector<std::string_view>::iterator end, const char *pattern, int patternSize, int offset = 0)
-{
-    std::vector<std::tuple<int, int, std::string_view>> results;
-    int lineNumber = 0;
-    for (auto &it = begin; it != end; ++it)
+    MTMathResults results;
+    long offset = 0;
+    for (auto&& future : futures)
     {
-        const char *text = it->data();
-        int textSize = static_cast<int>(it->size());
-        while (auto result = bruteforcematch(text, textSize, pattern, patternSize))
-        {
-            int pos = static_cast<int>(result - it->data());
-            results.push_back({lineNumber + offset, pos, {result, static_cast<std::string_view::size_type>(patternSize)}});
-            textSize -= ((result + patternSize) - text);
-            text = result + patternSize;
-        }
-        ++lineNumber;
-    }
-    return results;
-}
+        auto&& [n, r] = future.get();
 
-std::vector<std::tuple<int, int, std::string_view>> 
-find(std::vector<std::string_view> &lines, const char *pattern, int parts = 1)
-{
-    std::vector<std::tuple<int, int, std::string_view>> results;
-
-    auto patternSize = strlen(pattern);
-
-    parts = std::min(parts, static_cast<int>(lines.size()));
-    int partSize = static_cast<int>(lines.size() / parts);
-
-    auto it = lines.begin();
-    int offset = 0;
-
-    std::vector<std::future<std::vector<std::tuple<int, int, std::string_view>>>> futures;
-    for (auto p = 0; p < parts - 1; ++p, it += partSize, offset += partSize)
-    {
-        futures.push_back(std::async(std::launch::async, findInRange, it, it + partSize, pattern, patternSize, offset));
-    }
-    futures.push_back(std::async(std::launch::async, findInRange, it, lines.end(), pattern, patternSize, offset));
-
-    for (auto &&future : futures)
-    {
-        auto r = future.get();
+        for (auto& [s, p, str] : r) s += offset;
+        
+        offset += n;
 
         results.reserve(results.size() + r.size());
         std::move(std::begin(r), std::end(r), std::back_inserter(results));
     }
-
     return results;
 }
 
@@ -150,38 +141,28 @@ int main(int argc, char *argv[])
     if (argc < 3)
     {
         usage(argv[0]);
-        return static_cast<int>(MTFindResult::FAILURE);
+        return 1;
     }
 
     std::cerr << "Find \"" << argv[2] << "\" in file " << argv[1] << std::endl;
 
-    char *buffer = 0;
-    auto result = readFileToBuffer(argv[1], buffer);
+    char *text = 0;
+    long textSize = readFileToBuffer(argv[1], &text);
 
-    if (result != MTFindResult::OK)
+    if (textSize < 0)
     {
         std::cerr << "Can't open file: " << argv[1] << std::endl;
-        return static_cast<int>(result);
+        return 2;
     }
 
-    std::vector<std::string_view> lines;
-    splitBufferToLines(buffer, '\n', lines);
-
-    std::vector<std::tuple<int, int, std::string_view>> results;
-    if (lines.size() > 0)
+    MTMathResults mathResults = find(text, textSize, argv[2], strlen(argv[2]), 6);
+    std::cout << '\n' << mathResults.size() << std::endl;
+    for (auto &[line, pos, str] : mathResults)
     {
-        results = find(lines, argv[2], 8);
+        std::cout << (line + 1) << " " << (pos + 1) << " " << str << "\n";
     }
 
-    {
-        std::cout << results.size() << std::endl;
-        for (auto &[line, pos, str] : results)
-        {
-            std::cout << line + 1 << " " << pos + 1 << " " << str << "\n";
-        }
-    }
+    free(text);
 
-    free(buffer);
-
-    return static_cast<int>(MTFindResult::OK);
+    return 0;
 }
